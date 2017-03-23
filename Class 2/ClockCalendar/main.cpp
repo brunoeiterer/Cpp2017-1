732cfc2d514817f412a1658a6d3ae33b644c449c
Unknown <brunoeiterer@gmail.com>
2017-03-23 13:30:54 -0300

    Implement readClock method

--- Class 2/ClockCalendar/main.cpp
+++ Class 2/ClockCalendar/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include "Clock.h"
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    Clock clock(10, 20, 30, false);
+    int hour, minute, second;
+    bool pm;
+    clock.readClock(hour, minute, second, pm);
+    if(pm == true){
+        cout << hour << ":" << minute << ":" << second << " PM" << endl;
+    }
+    else{
+        cout << hour << ":" << minute << ":" << second << " AM" << endl;
+    }
     return 0;
 }
--- Class 2/ClockCalendar/src/Clock.cpp
+++ Class 2/ClockCalendar/src/Clock.cpp
@@ -1,20 +1,36 @@
 #include "Clock.h"
 
-Clock::Clock( int hr,  int min, int sec, bool is_pm){
+Clock::Clock(int hr, int min, int sec, bool is_pm){
     setClock(hr,min,sec,is_pm);
 }
 
-Clock::setClock( int hr,  int min, int sec, bool is_pm){
+void Clock::setClock(int hr, int min, int sec, bool is_pm){
     this->hr = hr;
     this->min = min;
     this->sec = sec;
     this->is_pm = is_pm;
 }
 
-Clock::readClock(int& h, int& s, int& m, bool& pm){
-    cout << this->hr << ":" << this->min << ":" << this->sec << this->is_pm ? "pm" : "am" << endl;
+void Clock::readClock(int& h, int& m, int& s, bool& pm){
+    h = hr;
+    s = min;
+    m = sec;
+    pm = is_pm;
 }
 
-Clock::advance(){
+void Clock::advance(){
+    sec++;
+    if(sec == 60){
+        sec = 0;
+        min++;
+        if(min == 60){
+            min = 0;
+            hr++;
+            if(hr == 13){
+                hr = 1;
+                is_pm = !is_pm;
+            }
+        }
+    }
 
 }
